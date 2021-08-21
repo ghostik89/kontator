45c87636552294c377e4b5778935650776c6e2fe
Tarapatina Ekaterina <Ekaterina.Tarapatina@jetbrains.com>
2021-08-21 20:13:57 +0300

    [feat] add header styles

--- src/components/LandingPage/Header.jsx
+++ src/components/LandingPage/Header.jsx
@@ -3,17 +3,26 @@ import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
-import whiteLogo from '../../img/logo-white.svg'
 import {routes} from "../../helpers/routes";
-import {Link} from "react-router-dom";
+import {useHistory} from "react-router-dom";
 
 export default function Header() {
+    const history = useHistory()
+
+    const goToLogin = () => {
+        history.push(routes.login)
+    }
     return (
-        <Box sx={{ flexGrow: 1, mb: 2 }}>
-            <AppBar position="static">
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static" color={"inherit"} sx={{boxShadow: 'none'}}>
                 <Toolbar>
-                    <img src={whiteLogo} alt={'logo'}/>
-                    <Button color="inherit"><Link to={routes.login}>Login</Link></Button>
+                    <Button
+                        sx={{ ml: '95%' }}
+                        onClick={goToLogin}
+                        color={"primary"}
+                    >
+                        Войти
+                    </Button>
                 </Toolbar>
             </AppBar>
         </Box>
