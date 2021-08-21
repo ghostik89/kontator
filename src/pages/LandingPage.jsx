c45440a2a0c4a311f339ea93ea8b8d60685091fb
Tarapatina Ekaterina <Ekaterina.Tarapatina@jetbrains.com>
2021-08-21 20:14:08 +0300

    [feat] add styles for page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,23 +1,55 @@
 import React from "react"
-import {Container, Grid} from "@material-ui/core";
+import {Container, Grid, Typography} from "@material-ui/core";
 import Header from "../components/LandingPage/Header";
 import EventsCard from "../components/EventsCard";
 import NewsCard from "../components/LandingPage/NewsCard";
+import landingBody from "../img/landing-body.svg"
+import { styled } from '@material-ui/core/styles';
+import {Copyright} from "../components/Copyright";
+
+
+const StyledImg = styled('img')(({theme}) => ({
+    width: '100%',
+    marginBottom: theme.spacing(4)
+}));
 
 export const LandingPage = () => {
     return (
         <>
             <Header/>
+            <StyledImg src={landingBody} alt={'body'}/>
             <Container>
                 <Grid container spacing={2}>
                     <Grid item xs={9}>
-                        <EventsCard description={"МероприятиеМероприятиеМероприятие"} name={"Мероприятие"}/>
+                        <Typography component={'h2'} variant={'h3'} gutterBottom>Текущие мероприятия</Typography>
+                        <Grid container spacing={2}>
+                            {[0,1,2,3,4].map((elem) => (
+                                <Grid item key={`landing-event-card-${elem}`}>
+                                    <EventsCard
+                                        description={"МероприятиеМероприятиеМероприятие"}
+                                        name={"Мероприятие"}
+                                    />
+                                </Grid>
+
+                            ))}
+                        </Grid>
                     </Grid>
                     <Grid item xs={3}>
-                        <NewsCard name={"News"} description={"News"}/>
+                        <Typography component={'h2'} variant={'h4'} gutterBottom>
+                            Последние новости
+                        </Typography>
+                        {[0,1,2,3,4].map((elem) => (
+                            <NewsCard
+                                key={`news-card-${elem}`}
+                                name={"News"}
+                                description={"News"}
+                                time={'12.09.2021'}
+                            />
+                        ))}
                     </Grid>
                 </Grid>
             </Container>
+            <Copyright/>
         </>
     )
 };
