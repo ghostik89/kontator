9edc5732077e2c80f0999ca753f4c62b91c66cbe
Tarapatina Ekaterina <Ekaterina.Tarapatina@jetbrains.com>
2021-08-21 10:06:54 +0300

    [feat] add logo to header

--- src/components/LandingPage/Header.jsx
+++ src/components/LandingPage/Header.jsx
@@ -2,24 +2,24 @@ import * as React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import whiteLogo from '../../img/logo-white.png'
+import {useHistory} from "react-router-dom";
+import {routes} from "../../helpers/routes";
 
 export default function Header() {
+    const history = useHistory()
+
+    const toLogin = () => {
+        history.push(routes.login)
+    }
+
     return (
         <Box sx={{ flexGrow: 1, mb: 2 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <Typography
-                        variant="h6"
-                        component="div"
-                        sx={{
-                            flexGrow: 1
-                        }}
-                    >
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
+                    <img src={whiteLogo} alt={'logo'}/>
+                    <Button color="inherit" onClick={toLogin}>Login</Button>
                 </Toolbar>
             </AppBar>
         </Box>
