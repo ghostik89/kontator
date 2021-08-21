d14f1c92137a0a6520105b6c377cf4064e5dd206
Tarapatina Ekaterina <Ekaterina.Tarapatina@jetbrains.com>
2021-08-21 15:18:06 +0300

    [init] add main page

--- src/pages/ResidentPages/Main.jsx
+++ src/pages/ResidentPages/Main.jsx
@@ -1,9 +1,31 @@
 import React from "react"
+import {Avatar, ListItem, ListItemAvatar, ListItemText, Typography} from "@material-ui/core";
+import NavBar from "../../components/ResidentPages/NavBar";
+import List from "@material-ui/core/List";
 
 export const ResidentMainPage = () => {
     return(
         <>
-
+            <Avatar/>
+            <NavBar>
+                <>
+                    <Avatar/>
+                    <Typography>Фамилия Имя Отчество</Typography>
+                    <Typography>Ваша команда</Typography>
+                    <List>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar/>
+                            </ListItemAvatar>
+                            <ListItemText primary={`Новая штука`} secondary={`менеджер`}/>
+                        </ListItem>
+                    </List>
+                    <Typography>
+                        Ваша текущая площадь
+                    </Typography>
+                    <Typography>Тэги</Typography>
+                </>
+            </NavBar>
         </>
     )
 }
