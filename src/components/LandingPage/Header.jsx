0281a461951804769e7ee489f8a43d6573e96fa8
Tarapatina Ekaterina <Ekaterina.Tarapatina@jetbrains.com>
2021-08-21 15:17:20 +0300

    [feat] add clickable header

--- src/components/LandingPage/Header.jsx
+++ src/components/LandingPage/Header.jsx
@@ -3,23 +3,19 @@ import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
-import whiteLogo from '../../img/logo-white.png'
-import {useHistory} from "react-router-dom";
+import whiteLogo from '../../img/logo-white.svg'
 import {routes} from "../../helpers/routes";
+import {Link} from "react-router-dom";
 
 export default function Header() {
-    const history = useHistory()
 
-    const toLogin = () => {
-        history.push(routes.login)
-    }
 
     return (
         <Box sx={{ flexGrow: 1, mb: 2 }}>
             <AppBar position="static">
                 <Toolbar>
                     <img src={whiteLogo} alt={'logo'}/>
-                    <Button color="inherit" onClick={toLogin}>Login</Button>
+                    <Button color="inherit"><Link to={routes.login}>Login</Link></Button>
                 </Toolbar>
             </AppBar>
         </Box>
