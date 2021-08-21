138dea6094926a4e67fd01459d020021b99a3c86
Tarapatina Ekaterina <Ekaterina.Tarapatina@jetbrains.com>
2021-08-21 20:04:58 +0300

    [init] create basic theme

---
+++ src/helpers/customTheme.js
@@ -0,0 +1,12 @@
+import { createTheme } from '@material-ui/core/styles';
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#614BF2',
+        },
+        secondary: {
+            main: '#FD1A62',
+        },
+    },
+});
